f0be41cedb6c67f7ef07b5ce6d296b4d98e70fb1
ahmedomi <ahmedomarmiledi@gmail.com>
2020-11-30 21:10:49 +0100

    calculate shape

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""how to calculate a shape of matrix"""
+
+
+def matrix_shape(matrix):
+    """shape of matrix"""
+    x = [len(matrix)]
+    while isinstance(matrix[0], list):
+        x.append(len(matrix[0]))
+        matrix = matrix[0]
+    return x
