424829950919b70b53c40d6b002107df6ed40ce4
ahmedomi <ahmedomarmiledi@gmail.com>
2021-01-04 20:37:17 +0100

    update class neuron and fixed error

--- supervised_learning/0x01-classification/1-neuron.py
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
-"""import numpy"""
+"""
+add class
+"""
+
 import numpy as np
-"""Create a Class Neuron Network with private attribute"""
 
 
 class Neuron:
+    """neuron class"""
+
     def __init__(self, nx):
-        if type(nx) is not int:
-            raise TypeError("nx must be an integer")
+        """constructor"""
+        if not type(nx) is int:
+            raise TypeError('nx must be an integer')
         if nx < 1:
-            raise ValueError("nx must be a positive integer")
-        self.__W = np.random.rand(1, nx)
-        self.__A = 0
+            raise ValueError('nx must be a positive integer')
+
+        self.__W = np.random.randn(1, nx)
         self.__b = 0
+        self.__A = 0
 
     @property
     def W(self):
         return self.__W
 
     @property
-    def A(self):
-        return self.__A
-
-    @property
     def b(self):
         return self.__b
+
+    @property
+    def A(self):
+        return self.__A
