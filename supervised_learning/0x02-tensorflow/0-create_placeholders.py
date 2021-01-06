04e8041c4bbc8068348195b90e113073b3629c62
ahmedomi <ahmedomarmiledi@gmail.com>
2021-01-06 17:15:34 +0100

    fixed code style and errors

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-install and import tensorflow library with pip install tensorflow
+module to create tensorflow placeholders
 """
 
 
@@ -9,7 +9,7 @@ import tensorflow as tf
 
 def create_placeholders(nx, classes):
     """
-    create placeholders with nx and classes parameters and x,y returns variable
+    create placeholders
     """
     x = tf.placeholder(name='x', dtype=tf.float32, shape=(None, nx))
     y = tf.placeholder(name='y', dtype=tf.float32, shape=(None, classes))
