d34eab9227157ea144bcdaaeba7ba9036a608816
ahmedomi <ahmedomarmiledi@gmail.com>
2021-01-21 18:59:31 +0100

    update functions

--- supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -7,23 +7,18 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """
     gradient descent
     """
-    m = Y.shape[1]
-    Z = cache["A" + str(L)] - Y
-    for count in range(L, 0, -1):
-        B = "b" + str(count)
-
-        W = "W" + str(count)
-        A = "A" + str(count - 1)
-
-        CA = cache[A]
-        dw = ((1 / m) * np.matmul(Z, CA.T))
-        db = (1 / m) * np.sum(Z, axis=1, keepdims=True)
-
-        zb = 1 - np.power(CA, 2)
-        if count > 1:
-            Z = Z.T * zb
-            Z = Z / keep_prob
-        else:
-            Z = cache["A" + str(L)] - Y
-        weights[W] = weights[W] - dw * alpha
-        weights[B] = weights[B] - db * alpha
+    copy_w = weights.copy()
+    d_z = cache["A" + str(L)] - Y
+    m = len(Y[0])
+    for i in range(L, 0, -1):
+        b = "b" + str(i)
+        w = "W" + str(i)
+        d = "D" + str(i - 1)
+        Actv = cache["A" + str(i - 1)]
+        d_b = (1 / m) * np.sum(d_z, axis=1, keepdims=True)
+        d_w = (1 / m) * np.matmul(d_z, Actv.T)
+        weights[w] = weights[w] - alpha * d_w
+        weights[b] = weights[b] - alpha * d_b
+        da = 1 - Actv * Actv
+        if i > 1:
+            d_z = np.matmul(copy_w[w].T, d_z) * da * cache[d] / keep_prob
