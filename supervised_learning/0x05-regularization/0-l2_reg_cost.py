0068534495a4bb5619189ee34469e4570360394e
ahmedomi <ahmedomarmiledi@gmail.com>
2021-01-21 18:35:15 +0100

    update task 0

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -4,14 +4,14 @@ calculates the cost of a neural network with L2 regularization
 """
 
 
-import numpy.linalg as nl
+import numpy as np
 
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """return the cost"""
     w = 0
     for i in range(1, L + 1):
-        w = w + nl.norm(weights['W' + str(i)])
+        w = w + np.linalg.norm(weights['W' + str(i)])
     L2 = cost + (lambtha * (1 / (2 * m))) * w
     return L2
 
