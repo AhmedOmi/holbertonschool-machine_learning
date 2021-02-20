ea13cbf6c9616c60937858ba740c346063ae890a
ahmedomi <ahmedomarmiledi@gmail.com>
2021-02-20 11:10:15 +0100

    fixed pycodestyle function

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
+"""create a neural network model"""
 
 
-import tensorflow.keras as tf
+import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    model.add(K.layers.Dense(units=layers[0],
+    """build a neural network models"""
+    reg = K.regularizers.l2
+    model = K.Sequential()
+    model.add(K.layers.Dense(layers[0], input_shape=(nx,),
                              activation=activations[0],
-                             kernel_regularizer=regularizer,
-                             input_shape=(nx,)))
-
-    # creating the subsequent densley-connected layers
-    for i in range(1, len(layers)):
+                             kernel_regularizer=reg(lambtha)))
+    for layer, act in zip(layers[1:], activations[1:]):
         model.add(K.layers.Dropout(1 - keep_prob))
-        model.add(K.layers.Dense(units=layers[i],
-                                 activation=activations[i],
-                                 kernel_regularizer=regularizer))
+        model.add(K.layers.Dense(layer, activation=act,
+                                 kernel_regularizer=reg(lambtha)))
     return model
