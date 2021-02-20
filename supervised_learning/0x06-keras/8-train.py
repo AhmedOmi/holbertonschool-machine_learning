07ae64a349542f3615c1620d04535b42b6e5ca39
ahmedomi <ahmedomarmiledi@gmail.com>
2021-02-20 11:26:32 +0100

    fixed pycodestyle

--- supervised_learning/0x06-keras/8-train.py
+++ supervised_learning/0x06-keras/8-train.py
@@ -15,23 +15,24 @@ def train_model(network, data, labels,
     def scheduler(epoch):
         """function scheduler"""
         return alpha / (1 + decay_rate * epoch)
+
     x = []
     if early_stopping is True:
         p = K.callbacks.EarlyStopping(monitor='val_loss',
-                                          patience=patience)
+                                      patience=patience)
         x.append(p)
     if learning_rate_decay and validation_data:
         p = K.callbacks.LearningRateScheduler(scheduler,
-                                                verbose=1)
+                                              verbose=1)
         x.append(p)
     if save_best and validation_data:
         a = K.callbacks.ModelCheckpoint(filepath, save_best_only=True)
         x.append(a)
     re = network.fit(x=data, y=labels,
-                          callbacks=x,
-                          epochs=epochs,
-                          batch_size=batch_size,
-                          validation_data=validation_data,
-                          verbose=verbose,
-                          shuffle=shuffle)
+                     callbacks=x,
+                     epochs=epochs,
+                     batch_size=batch_size,
+                     validation_data=validation_data,
+                     verbose=verbose,
+                     shuffle=shuffle)
     return re
