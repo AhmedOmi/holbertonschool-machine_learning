a93ff0c36aae979fcfc6ce09b64e958f2ec70c9b
ahmedomi <ahmedomarmiledi@gmail.com>
2021-02-24 13:34:25 +0100

    update function

--- supervised_learning/0x07-cnn/3-pool_backward.py
+++ supervised_learning/0x07-cnn/3-pool_backward.py
@@ -1,10 +1,38 @@
 #!/usr/bin/env python3
-"""Fix comment"""
-
-
+""" Pooling Backpropagation """
 import numpy as np
 
 
-def pool_backward(dZ, A_prev, kernel_shape, stride=(1, 1), mode='max'):
-    """Fix comment"""
-    return None
+def pool_backward(dA, A_prev, kernel_shape,
+                  stride=(1, 1), mode='max'):
+    """ pooling layer of function backward """
+    m, h_new, w_new, c_new = dA.shape
+    m, h_prev, w_prev, c = A_prev.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+
+    dA_prev = np.zeros(A_prev.shape)
+    for z in range(m):
+        for i in range(h_new):
+            for j in range(w_new):
+                for k in range(c_new):
+                    h_init = i * sh
+                    h_end = i * sh + kh
+                    w_init = j * sw
+                    w_end = j * sw + kw
+                    if mode == 'max':
+                        value = np.max(A_prev[z,
+                                              h_init:h_end,
+                                              w_init:w_end,
+                                              k])
+                        mask = np.where(A_prev[z,
+                                               h_init:h_end,
+                                               w_init:w_end,
+                                               k] == value, 1, 0)
+                        mask = mask * dA[z, i, j, k]
+                    elif mode == 'avg':
+                        mask = np.ones(kernel_shape) * (dA[z, i, j, k] /
+                                                        (kh * kw))
+                    dA_prev[z, h_init:h_end,
+                            w_init:w_end, k] += mask
+    return dA_prev
--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -1,31 +1,40 @@
 #!/usr/bin/env python3
-"""Implement a modified LeNet-5 architecture using Tensorflow"""
-
-
+""" LeNet-5 (Tensorflow) """
 import tensorflow as tf
 
 
-def lenet5(X, Y):
-    """Implement a modified LeNet-5 architecture using Tensorflow"""
-    initializer = (tf.contrib.layers.
-                   variance_scaling_initializer())
-    out = tf.layers.Conv2D(6, 5, padding='same',
-                           activation='relu',
-                           kernel_initializer=initializer)(X)
-    out = tf.layers.MaxPooling2D(2, 2)(out)
-    out = tf.layers.Conv2D(16, 5, padding='valid', activation='relu',
-                           kernel_initializer=initializer)(out)
-    out = tf.layers.MaxPooling2D(2, 2)(out)
-    out = tf.layers.Flatten()(out)
-    out = tf.layers.Dense(120, activation='relu',
-                          kernel_initializer=initializer)(out)
-    out = tf.layers.Dense(84, activation='relu',
-                          kernel_initializer=initializer)(out)
-    out = tf.layers.Dense(10, activation='softmax',
-                          kernel_initializer=initializer)(out)
-    loss = tf.losses.softmax_cross_entropy(Y, out)
+def lenet5(x, y):
+    """ builds a modified version of the LeNet-5 architecture using tensorflow """
+    he_normal = tf.contrib.layers.variance_scaling_initializer()
+    relu = tf.nn.relu
+    conv_lay1 = tf.layers.Conv2D(filters=6,
+                                 kernel_size=(5, 5),
+                                 padding='same',
+                                 activation=relu,
+                                 kernel_initializer=he_normal)(x)
+    pool_lay1 = tf.layers.MaxPooling2D(pool_size=(2, 2),
+                                       strides=(2, 2))(conv_lay1)
+    conv_lay2 = tf.layers.Conv2D(filters=16,
+                                 kernel_size=(5, 5),
+                                 padding='valid',
+                                 activation=relu,
+                                 kernel_initializer=he_normal)(pool_lay1)
+    pool_lay2 = tf.layers.MaxPooling2D(pool_size=(2, 2),
+                                       strides=(2, 2))(conv_lay2)
+    flatten = tf.layers.Flatten()(pool_lay2)
+    full_lay3 = tf.layers.Dense(units=120,
+                                activation=relu,
+                                kernel_initializer=he_normal)(flatten)
+    full_lay4 = tf.layers.Dense(units=84,
+                                kernel_initializer=he_normal)(full_lay3)
+    softmax = tf.layers.Dense(units=10,
+                              kernel_initializer=he_normal)(full_lay4)
+    loss = tf.losses.softmax_cross_entropy(y, logits=softmax)
+    y_max = tf.argmax(y, axis=1)
+    y_pred_max = tf.argmax(activated_sof, axis=1)
+    bias = tf.cast(tf.equal(y_max, y_pred_max), dtype=tf.float32)
+    accuracy = tf.reduce_mean(bias)
     train = tf.train.AdamOptimizer().minimize(loss)
-    max_pred = tf.argmax(out, 1)
-    equal = tf.equal(tf.argmax(Y, 1), max_pred)
-    accuracy = tf.reduce_mean(tf.cast(equal, tf.float32))
-    return out, train, loss, accuracy
+    activated_sof = tf.nn.softmax(softmax)
+
+    return activated_sof, train, loss, accuracy
