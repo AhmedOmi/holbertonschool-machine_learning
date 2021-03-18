72a61849b48e9a0f5c4097d2c4c915a56210d91d
AhmedOmi <ahmedomarmiledi@gmail.com>
2021-03-18 18:33:18 +0100

    update

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -3,5 +3,7 @@ import numpy as np
 
 
 def determinant(matrix):
+    if matrix is not list:
+        raise TypeError('matrix must be a list of lists')
     deter = np.linalg.det(matrix)
     return deter
