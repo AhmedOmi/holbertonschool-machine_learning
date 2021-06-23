6ef0845155b9186e097fd36af4fd3171e0c90851
AhmedOmi <ahmedomarmiledi@gmail.com>
2021-06-23 15:46:21 +0100

    env.reset() it s for initialise the environment

--- reinforcement_learning/0x00-q_learning/0-load_env.py
+++ reinforcement_learning/0x00-q_learning/0-load_env.py
@@ -17,5 +17,5 @@ def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     """
     env = gym.make(id='FrozenLake-v0', desc=desc,
                    map_name=map_name, is_slippery=is_slippery)
-    env.reset()
+    env.reset()  # initialise the environment
     return env
